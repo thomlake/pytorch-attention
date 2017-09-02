20b31aa5faa155639df8c206de2864af80924254
thomlake <thom.l.lake@gmail.com>
2017-09-02 17:53:25 -0500

    add setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='attention',
+    version='0.1.0',
+    author='tllake',
+    author_email='thom.l.lake@gmail.com',
+    packages=['attention'],
+    description='An attention function for PyTorch.',
+    long_description=open('README.md').read())
